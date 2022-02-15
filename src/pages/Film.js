cb15930382526d2d8ab2b15b0c4b5ebdfbabf107
pauline-wim <linette.wim@hotmail.com>
2022-02-15 16:43:14 +0100

    component Film has now access to data from catalog.json

--- src/pages/Film.js
+++ src/pages/Film.js
@@ -1,30 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // JSON
 import Catalog from "../catalog.json";
 
 class Film extends React.Component {
-  constructor() {
-    super();
-
-    // this.getFilm = this.getFilm.bind(this);
-  }
+  // constructor() {
+  //   super();
+  // }
 
   render() {
+    const pageID = this.props.match.params.id;
     return (
       <article>
         <h1>Film</h1>
-        <p>id: {this.props.match.params.id}</p>
-        <p>
-          <span>Title:</span>{" "}
-          {Catalog.map((film) => {
-            return (
-              <div>
-                <p>id: {this.props.match.params.id}</p>
-                <p>title: {film.title}</p>
-              </div>
-            );
-          })}
-        </p>
+        <p>id: {pageID}</p>
+        <div key={Catalog[pageID - 1].id}>
+          <p>
+            <span>Title:</span> {Catalog[pageID - 1].title}
+          </p>
+          <p>
+            <span>Director:</span> {Catalog[pageID - 1].director}
+          </p>
+          <p>
+            <span>Actors:</span>{" "}
+          </p>
+          <ul>
+            {Catalog[pageID - 1].stars.map((el, i) => {
+              return <li key={i}>{el}</li>;
+            })}
+          </ul>
+          <img src={Catalog[pageID - 1].image} alt="film poster" />
+          <p>
+            <span>Description:</span> {Catalog[pageID - 1].description}
+          </p>
+        </div>
+        <button>
+          <Link to="/">Back to Homepage</Link>
+        </button>
       </article>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,23 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Catalog from "../catalog.json";
 
 class Home extends React.Component {
-  constructor() {
-    super();
-
-    // this.getFilm = this.getFilm.bind(this);
-  }
-
-  // getFilm() {
-  //   Catalog.map((film) => {
-  //     return (
-  //       <div>
-  //         <p>id: {this.props.match.params.id}</p>
-  //         <p>title: {film.title}</p>
-  //       </div>
-  //     );
-  //   });
-  // }
-
   render() {
     return (
       <div>
         <h1>Home</h1>
-        {Catalog.map((film) => {
+        {Catalog.map((film, i) => {
           return (
-            <div>
-              <p>id: {film.id}</p>
-              <p>title: {film.title}</p>
+            <div key={i}>
+              <p>
+                <Link to={`/film/${film.id}`}>
+                  {film.id} {film.title}
+                </Link>
+              </p>
             </div>
           );
         })}
-        {/* <Link to="/film/:id">{this.getFilm}</Link> */}
       </div>
     );
   }
